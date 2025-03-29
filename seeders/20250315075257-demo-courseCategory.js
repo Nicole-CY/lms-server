76d1ab607b04b6c08d8d5e453ad2783beaf87388
Nicole L <nicole.chunyang.li@gmail.com>
2025-03-29 16:59:31 +1100

    Updated categoryId values

--- seeders/20250315075257-demo-courseCategory.js
+++ seeders/20250315075257-demo-courseCategory.js
@@ -1,25 +1,33 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("course_category", [
-      {
-        courseId: 1,  // Associates with the first course
-        categoryId: 101  // Example category id
-      },
-      {
-        courseId: 1,
-        categoryId: 102  // Another category for the first course
-      },
-      {
-        courseId: 2,
-        categoryId: 101  // First course category also assigned to the second course
-      }
-    ], {});
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "course_category",
+      [
+        {
+          courseId: 1, // "Introduction to Node.js"
+          categoryId: 8, // "Node.js"
+        },
+        {
+          courseId: 1,
+          categoryId: 6, // "JavaScript"
+        },
+        {
+          courseId: 2, // "Advanced Sequelize"
+          categoryId: 1, // "Programming"
+        },
+        {
+          courseId: 2,
+          categoryId: 4, // "Data Science"
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete("course_category", null, {});
-  }
+  },
 };
