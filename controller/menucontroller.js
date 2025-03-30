551eb8a87e3c76e7656cf2c32397a5c47369f48d
Nicole L <nicole.chunyang.li@gmail.com>
2025-03-31 03:57:07 +1100

    fix duplicate calls in migrations

--- controller/menucontroller.js
+++ controller/menucontroller.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const menuService = require("../service/menuService");
-=======
-const menuService = require('../service/menuService');
->>>>>>> dev
 
 /**
  * create a new menu
@@ -10,19 +6,6 @@ const menuService = require('../service/menuService');
  * @param {*} res
  */
 const createMenuAsync = async (req, res) => {
-<<<<<<< HEAD
-  let menu = {};
-  menu.menuName = req.body.menuName;
-  menu.parentId = req.body.parentId;
-  menu.routePath = req.body.routePath;
-
-  let result = await menuService.createMenuAsync(menu);
-  if (result.isSuccess) {
-    res.sendCommonValue(result.data, "success", 1);
-  } else {
-    res.sendCommonValue({}, "Create menu failed", 0);
-  }
-=======
     const parentId = req.body.parentId;
 
     // check if parent ID exist
@@ -41,11 +24,10 @@ const createMenuAsync = async (req, res) => {
 
     let result = await menuService.createMenuAsync(menu);
     if (result.isSuccess) {
-        res.sendCommonValue(result.data, 'success', 1);
+        res.sendCommonValue(result.data, "success", 1);
     } else {
-        res.sendCommonValue({}, 'Create menu failed', 0);
+        res.sendCommonValue({}, "Create menu failed", 0);
     }
->>>>>>> dev
 };
 
 /**
@@ -54,19 +36,6 @@ const createMenuAsync = async (req, res) => {
  * @param {*} res
  */
 const getMenuAsync = async (req, res) => {
-<<<<<<< HEAD
-  let result = await menuService.getMenuAsync();
-  if (result.isSuccess) {
-    res.sendCommonValue(result.data, "success", 1);
-  } else {
-    res.sendCommonValue({}, "Get menu failed", 0);
-  }
-};
-
-module.exports = {
-  createMenuAsync,
-  getMenuAsync,
-=======
     let result = await menuService.getMenuAsync();
     if (result.isSuccess) {
         res.sendCommonValue(result.data, 'success', 1);
@@ -78,5 +47,4 @@ module.exports = {
 module.exports = {
     createMenuAsync,
     getMenuAsync,
->>>>>>> dev
 };
--- migrations/20250315123558-create-courseOffering.js
+++ migrations/20250315123558-create-courseOffering.js
@@ -1,49 +1,9 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.createTable('course_offering', {
-            id: {
-                type: Sequelize.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false,
-            },
-            course_instance_id: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-                references: {
-                    model: 'course_instance',
-                    key: 'id',
-                },
-                onDelete: 'CASCADE',
-            },
-            teacher_id: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-                // references: {
-                //   model: "User",
-                //   key: "id",
-                // },
-                // onDelete: "SET NULL",
-            },
-            start_date: {
-                type: Sequelize.DATE,
-                allowNull: false,
-            },
-            student_capacity: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-            },
-            status: {
-                type: Sequelize.ENUM('Pending Start', 'Active', 'Completed'),
-                allowNull: false,
-            },
-        });
-    },
-
-    await queryInterface.createTable('course_offering', {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("course_offering", {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
@@ -63,7 +23,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: true,
         references: {
-          model: "user",
+          model: "user", // Ensure this matches your actual table name
           key: "id",
         },
         onDelete: "SET NULL",
@@ -83,17 +43,17 @@ module.exports = {
       created_at: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updated_at: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
     });
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("course_offering");
-  }
+  },
 };
