b474c7d96e38bae41df5a114216b1fb7533ca458
Ray Runze Wang <rayrunzewang@gmail.com>
2025-04-01 20:24:15 +1100

    fix(table): unify naming case and setup created_at and updated_at

--- migrations/20250315072550-create-courseInstance.js
+++ migrations/20250315072550-create-courseInstance.js
@@ -2,67 +2,67 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.createTable("course_instance", {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      course_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "course",
-          key: "id"
-        },
-        onDelete: "CASCADE"
-      },
-      start_date: {
-        type: Sequelize.DATE,
-        allowNull: true,
-      },
-      end_date: {
-        type: Sequelize.DATE,
-        allowNull: true,
-      },
-      total_sessions: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-      },
-      launch_status: {
-        type: Sequelize.ENUM("Scheduled", "In Progress", "Completed", "Cancelled"),
-        allowNull: true,
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('course_instance', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            course_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'course',
+                    key: 'id',
+                },
+                onDelete: 'CASCADE',
+            },
+            start_date: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+            end_date: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+            total_sessions: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+            },
+            launch_status: {
+                type: Sequelize.ENUM('Scheduled', 'In Progress', 'Completed', 'Cancelled'),
+                allowNull: true,
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+            updated_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+            created_by: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'user',
+                    key: 'id',
+                },
+                onDelete: 'CASCADE',
+            },
+            updated_by: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'user',
+                    key: 'id',
+                },
+                onDelete: 'CASCADE',
+            },
+        });
     },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-    },
-    created_by: {
-      type: Sequelize.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
-      onDelete: "CASCADE",
-    },
-    updated_by: {
-      type: Sequelize.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
-      onDelete: "CASCADE",
-    },
-    });
-  },
 
     async down(queryInterface, Sequelize) {
         await queryInterface.dropTable('course_instance');
