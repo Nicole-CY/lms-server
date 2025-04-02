11db690047b23d2f3517397db419a041ac47e6aa
Ray Runze Wang <rayrunzewang@gmail.com>
2025-04-02 11:34:19 +1100

    fix(login): user service login by email error

--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,16 @@
-const userService = require("../service/userService");
-const bcrypt = require("bcryptjs");
-const { bcryptConfig } = require("../appConfig");
+const bcrypt = require('bcryptjs');
+
+const userService = require('../service/userService');
+const { bcryptConfig } = require('../appConfig');
 
 const addUserAsync = async (req, res) => {
-    const existingEmail = await userService.getUserbyEmailAsync(req.body.email);
+    const existingEmail = await userService.getUserByEmailAsync(req.body.email);
 
     if (existingEmail.isSuccess && existingEmail.data.id > 0) {
-        return res.sendCommonValue({}, "Email already exists", 400, 400);
+        return res.sendCommonValue({}, 'Email already exists', 400, 400);
     }
 
-    let user = {};
+    const user = {};
     user.password = req.body.password;
     user.email = req.body.email;
     user.firstName = req.body.firstName;
@@ -21,57 +22,53 @@ const addUserAsync = async (req, res) => {
     user.roles = req.body.roles;
     user.active = req.body.active;
 
-    let password = req.body.password;
+    const password = req.body.password;
     const salt = await bcrypt.genSalt(bcryptConfig.saltRounds);
-    let encrypPassword = await bcrypt.hash(user.password, salt);
+    const encrypPassword = await bcrypt.hash(user.password, salt);
     user.password = encrypPassword;
-    let result = await userService.addUserAsync(user);
+    const result = await userService.addUserAsync(user);
     if (result.isSuccess) {
         user.password = password;
-        res.sendCommonValue(user, "success", 1);
+        res.sendCommonValue(user, 'success', 1);
     } else {
-        res.sendCommonValue({}, "failed", 0);
+        res.sendCommonValue({}, 'failed', 0);
     }
 };
-
-
 
 const getUserListAsync = async (req, res) => {
-    let page = parseInt(req.query.page);
-    let pageSize = parseInt(req.query.pageSize);
-    let result = await userService.getUserListAsync(page, pageSize);
+    const page = parseInt(req.query.page);
+    const pageSize = parseInt(req.query.pageSize);
+    const result = await userService.getUserListAsync(page, pageSize);
     if (result.isSuccess) {
-        res.sendCommonValue(result.data, "success", 1);
+        res.sendCommonValue(result.data, 'success', 1);
     } else {
-        res.sendCommonValue([], "failed", 0);
+        res.sendCommonValue([], 'failed', 0);
     }
 };
 
-const getUserByNameAsync = async (req,res) => {
+const getUserByNameAsync = async (req, res) => {
     const username = req.params.userName;
-    const result = await userService.getUserbyNameAsync(username)
-    if(result.isSuccess){
-        res.sendCommonValue(result.data,'success', 1)
+    const result = await userService.getUserbyNameAsync(username);
+    if (result.isSuccess) {
+        res.sendCommonValue(result.data, 'success', 1);
     } else {
-        res.sendCommonValue({}, result.message, 0)
+        res.sendCommonValue({}, result.message, 0);
     }
-}
+};
 
 const getUserByIdAsync = async (req, res) => {
-    let id = parseInt(req.params.id);
-    let result = await userService.getUserbyIdAsync(id);
+    const id = parseInt(req.params.id);
+    const result = await userService.getUserbyIdAsync(id);
     if (result.isSuccess) {
-        res.sendCommonValue(result.data, "success", 1);
+        res.sendCommonValue(result.data, 'success', 1);
     } else {
-        res.sendCommonValue([], "failed", 0);
+        res.sendCommonValue([], 'failed', 0);
     }
 };
 
-
-
 const updateUserAsync = async (req, res) => {
-    //check username not in db
-    let user = {};
+    // check username not in db
+    const user = {};
     user.id = req.body.id;
     user.username = req.body.username;
     user.email = req.body.email;
@@ -84,36 +81,29 @@ const updateUserAsync = async (req, res) => {
     user.roles = req.body.roles;
     user.active = req.body.active;
 
-    let checkUserResult = await userService.checkUserNameAsync(
-        user.username,
-        user.id
-    );
+    const checkUserResult = await userService.checkUserNameAsync(user.username, user.id);
     if (!checkUserResult.isSuccess) {
-        res.sendCommonValue({}, "Username already exists", 400, 400);
+        res.sendCommonValue({}, 'Username already exists', 400, 400);
         return;
     }
-    let dbResult = await userService.updateUserByIdAsync(user);
+    const dbResult = await userService.updateUserByIdAsync(user);
     if (dbResult.isSuccess) {
-        res.sendCommonValue(user, "success", 1);
+        res.sendCommonValue(user, 'success', 1);
         return;
     } else {
-        res.sendCommonValue({}, "failed", 0);
+        res.sendCommonValue({}, 'failed', 0);
     }
 };
-
-
 
 const deleteUserByIdAsync = async (req, res) => {
-    let ids = req.params.ids;
-    let result = await userService.deleteUserByIdAsync(ids);
+    const ids = req.params.ids;
+    const result = await userService.deleteUserByIdAsync(ids);
     if (result.isSuccess) {
-        res.sendCommonValue({}, "success", 1);
+        res.sendCommonValue({}, 'success', 1);
     } else {
-        res.sendCommonValue({}, "failed", 0);
+        res.sendCommonValue({}, 'failed', 0);
     }
 };
-
-
 
 module.exports = {
     addUserAsync,
