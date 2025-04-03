1d2f96c9f1f5a910338384ba951512b521f62ee4
Ray Runze Wang <rayrunzewang@gmail.com>
2025-04-03 12:52:34 +1100

    fix(migration) ERROR: Cannot drop index 'unique_courseInstanceId_order': needed in a foreign key constraint

--- migrations/20250402092420-session-update-order.js
+++ migrations/20250402092420-session-update-order.js
@@ -2,28 +2,29 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn("session", "order", {
-      type: Sequelize.INTEGER,
-      allowNull: false, 
-      unique: false,   
-    });
+    async up(queryInterface, Sequelize) {
+        await queryInterface.addConstraint('session', {
+            fields: ['course_instance_id', 'order'],
+            type: 'unique',
+            name: 'unique_courseInstanceId_order',
+        });
+    },
 
-    await queryInterface.addConstraint("session", {
-      fields: ["course_instance_id", "order"], 
-      type: "unique",
-      name: "unique_courseInstanceId_order", 
-    });
-  },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.removeConstraint('session', 'session_ibfk_1');
 
-  async down (queryInterface, Sequelize) {
+        await queryInterface.removeIndex('session', 'unique_courseInstanceId_order');
 
-    await queryInterface.removeConstraint("session", "unique_courseInstanceId_order");
-
-    await queryInterface.changeColumn("session", "order", {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      unique: true, 
-    });
-  }
+        await queryInterface.addConstraint('session', {
+            fields: ['course_instance_id'],
+            type: 'foreign key',
+            name: 'session_ibfk_1',
+            references: {
+                table: 'course_instance',
+                field: 'id',
+            },
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+        });
+    },
 };
--- migrations/20250402093124-sessionMedia-update-path-length.js
+++ migrations/20250402093124-sessionMedia-update-path-length.js
@@ -2,27 +2,27 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn("media", "file_path", {
-      type: Sequelize.TEXT, 
-      allowNull: false, 
-    });
+    async up(queryInterface, Sequelize) {
+        await queryInterface.changeColumn('media', 'file_path', {
+            type: Sequelize.TEXT,
+            allowNull: false,
+        });
 
-    await queryInterface.changeColumn("media", "thumbnail_path", {
-      type: Sequelize.TEXT, 
-      allowNull: true, 
-    });
-  },
+        await queryInterface.changeColumn('media', 'thumbnail_path', {
+            type: Sequelize.TEXT,
+            allowNull: true,
+        });
+    },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn("media", "file_path", {
-      type: Sequelize.STRING(255), 
-      allowNull: false,
-    });
+    async down(queryInterface, Sequelize) {
+        await queryInterface.changeColumn('media', 'file_path', {
+            type: Sequelize.STRING(255),
+            allowNull: false,
+        });
 
-    await queryInterface.changeColumn("media", "thumbnail_path", {
-      type: Sequelize.STRING(255),  
-      allowNull: true,
-    });
-  }
+        await queryInterface.changeColumn('media', 'thumbnail_path', {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+        });
+    },
 };
