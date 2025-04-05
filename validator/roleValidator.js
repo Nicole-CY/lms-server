c98c922c1fb71074f040a0e09a856a0400087b45
FreyaXu <yinyue161425@gmail.com>
2025-04-05 19:54:31 +1100

    refactor: validation place of role crud; fix: some field naming problems

--- validator/roleValidator.js
+++ validator/roleValidator.js
@@ -1,20 +1,28 @@
-const { body } = require("express-validator");
+const { param, body } = require('express-validator');
 
-const roleValidator = [ 
-  body("role_name")
-    .notEmpty()
-    .withMessage("Role name is required")
-    .isString()
-    .withMessage("Role name must be a string")
-    .isLength({ min: 3, max: 50 })
-    .withMessage("Role name must be between 3 and 50 characters"),
+const roleValidator = [
+    body('role_name')
+        .notEmpty()
+        .withMessage('Role name is required')
+        .isString()
+        .withMessage('Role name must be a string')
+        .isLength({ min: 3, max: 50 })
+        .withMessage('Role name must be between 3 and 50 characters'),
 
-  body("description")
-    .optional()
-    .isString()
-    .withMessage("Description must be a string")
-    .isLength({ max: 500 })
-    .withMessage("Description cannot exceed 500 characters"),
+    body('description')
+        .optional()
+        .isString()
+        .withMessage('Description must be a string')
+        .isLength({ max: 500 })
+        .withMessage('Description cannot exceed 500 characters'),
 ];
 
-module.exports = { roleValidator };
+const roleIdValidator = [
+    param('id')
+        .notEmpty()
+        .withMessage('Role ID is required')
+        .isInt({ min: 1 })
+        .withMessage('Role ID must be a positive integer'),
+];
+
+module.exports = { roleValidator, roleIdValidator };
