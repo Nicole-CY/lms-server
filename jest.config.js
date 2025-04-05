6ba7cea44f764644ab43901432b225e28683a604
Nicole L <nicole.chunyang.li@gmail.com>
2025-04-05 20:21:41 +0800

    update

--- jest.config.js
+++ jest.config.js
@@ -6,29 +6,27 @@ module.exports = {
     testTimeout: 10000,
     collectCoverage: true,
     collectCoverageFrom: [
-      '!**/node_modules/**',
-      '!**/tests/**',
-      '!**/migrations/**',
-      '!**/seeders/**',
-      '!**/config/**',
-      '!**/index.js',
+        '!**/node_modules/**',
+        '!**/tests/**',
+        '!**/migrations/**',
+        '!**/seeders/**',
+        '!**/config/**',
+        '!**/index.js',
     ],
     coverageDirectory: 'coverage',
     reporters: ['default'],
-  
     projects: [
-      {
-        displayName: 'unit',
-        testEnvironment: 'node',
-        // setupFilesAfterEnv: ['<rootDir>/jest/setupUnitTest.js'],
-        testMatch: ['<rootDir>/test/unit/**/*.{test,spec}.js']
-      },
-      {
-        displayName: 'integration',
-        testEnvironment: 'node',
-        setupFilesAfterEnv: ['<rootDir>/jest/setupIntegrationTest.js'],
-        testMatch: ['<rootDir>/test/integration/**/*.{test,spec}.js']
-      }
-    ]
-  };
-  
+        {
+            displayName: 'unit',
+            testEnvironment: 'node',
+            // setupFilesAfterEnv: ['<rootDir>/jest/setupUnitTest.js'],
+            testMatch: ['<rootDir>/test/unit/**/*.{test,spec}.js'],
+        },
+        {
+            displayName: 'integration',
+            testEnvironment: 'node',
+            setupFilesAfterEnv: ['<rootDir>/jest/setupIntegrationTest.js'],
+            testMatch: ['<rootDir>/test/integration/**/*.{test,spec}.js'],
+        },
+    ],
+};
