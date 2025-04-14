1d75631359f294c80f7c34913d028cd51a0d0a48
Ray Runze Wang <rayrunzewang@gmail.com>
2025-04-14 21:19:50 +1000

    refactor(folder-structure): update folder structure for better architecture

--- jest.config.js
+++ jest.config.js
@@ -20,13 +20,13 @@ module.exports = {
             displayName: 'unit',
             testEnvironment: 'node',
             // setupFilesAfterEnv: ['<rootDir>/jest/setupUnitTest.js'],
-            testMatch: ['<rootDir>/test/unit/**/*.{test,spec}.js'],
+            testMatch: ['<rootDir>/tests/unit/**/*.{test,spec}.js'],
         },
         {
             displayName: 'integration',
             testEnvironment: 'node',
             setupFilesAfterEnv: ['<rootDir>/jest/setupIntegrationTest.js'],
-            testMatch: ['<rootDir>/test/integration/**/*.{test,spec}.js'],
+            testMatch: ['<rootDir>/tests/integration/**/*.{test,spec}.js'],
         },
     ],
 };
---
+++ src/config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const { mysqlConfig } = require('../../appConfig');
+
+module.exports = {
+    development: {
+        host: mysqlConfig.host,
+        port: mysqlConfig.port,
+        username: mysqlConfig.user,
+        password: mysqlConfig.password,
+        database: mysqlConfig.database,
+        dialect: 'mysql',
+        migrationStorageTableName: 'sequelize_meta',
+    },
+    test: {
+        host: mysqlConfig.host,
+        port: mysqlConfig.port,
+        username: mysqlConfig.user,
+        password: mysqlConfig.password,
+        database: mysqlConfig.database,
+        dialect: 'mysql',
+        migrationStorageTableName: 'sequelize_meta',
+    },
+    test: {
+        host: mysqlConfig.host,
+        port: mysqlConfig.port,
+        username: mysqlConfig.user,
+        password: mysqlConfig.password,
+        database: mysqlConfig.database,
+        dialect: 'mysql',
+    },
+};
