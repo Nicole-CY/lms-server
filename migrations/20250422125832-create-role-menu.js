0cc4cb1e6936e12c58bc62ca1d1dc3e064dc2cc9
Ray Runze Wang <rayrunzewang@gmail.com>
2025-04-26 21:26:46 +1000

    fix(migration): add created_at and updated_at in create-role-menu migration

--- migrations/20250422125832-create-role-menu.js
+++ migrations/20250422125832-create-role-menu.js
@@ -22,6 +22,16 @@ module.exports = {
                 },
                 onDelete: 'CASCADE',
             },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+            updated_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
         });
     },
 
