beb99750c93d6c407a9da31e3815b5c869162227
lily.jiang <jianglili2015@gmail.com>
2025-05-11 00:02:01 +1000

    set json body size in app.js for saving pictures via node.js

--- app.js
+++ app.js
@@ -20,8 +20,11 @@ const returnValue = require('./src/middlewares/returnValue');
 app.use(returnValue.returnValue);
 
 // config josn body
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+//app.use(express.urlencoded({ extended: false }));
+
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
